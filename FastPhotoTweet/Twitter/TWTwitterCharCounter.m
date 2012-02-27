287b59dc8116f6c7510022766646830506c336bc
ktysne <github@ktysne.info>
2012-02-27 15:42:22 +0900

    update
    ・変数名のミスを修正
    ・コメントを追加

--- FastPhotoTweet/Twitter/TWTwitterCharCounter.m
+++ FastPhotoTweet/Twitter/TWTwitterCharCounter.m
@@ -9,11 +9,15 @@
 
 @implementation TWTwitterCharCounter
 
+//現在入力されている文字数をt.coを考慮してカウントし、残りの入力可能文字数を返す
+//URLはどんな長さでも1つ20文字としてカウント
 + (int)charCounter:(id)post {
     
+    //自動開放プールを生成
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     
-    NSString *postString = [NSString stringWithString:post];
+    //id型で受け取りNSMutableStringにキャスト
+    NSMutableString *postString = [NSMutableString stringWithString:post];
     
     int num = 140;
     int originalCharNum = postString.length;
@@ -22,42 +26,51 @@
     
     @try {
         
+        //URLを抽出する正規表現を設定
         NSRegularExpression *regexp = [NSRegularExpression regularExpressionWithPattern:@"https?:([^\\x00-\\x20()\"<>\\x7F-\\xFF])*" 
                                                                                 options:0 
                                                                                   error:nil];
         
-        NSArray *match = [regexp matchesInString:post 
+        NSArray *match = [regexp matchesInString:postString 
                                          options:0 
                                            range:NSMakeRange(0, postString.length)];
         
+        //文字列中にいくつURLがあるかチェック
         for (NSTextCheckingResult *result in match) {
             
-            NSString *matchString = [post substringWithRange:result.range];
+            NSString *matchString = [postString substringWithRange:result.range];
             [urlList addObject:matchString];
+            
+            //マッチしたらカウントを増やす
             urlCount++;
             
         }
         
         for (NSString *tmp in urlList) {
             
-            [post replaceOccurrencesOfString:tmp withString:@"" 
+            //オリジナルの文字列からURLを取り除く
+            [postString replaceOccurrencesOfString:tmp withString:@"" 
                                      options:0 
                                        range:NSMakeRange(0, postString.length)];
             
         }
         
+        //残り入力可能文字数 = 140 - URL以外の文字数 - URLの数 * 20
         num = num - postString.length - urlCount * 20;
         
     }@catch (NSException *e) {
         
+        //エラーの場合は取り敢えずオリジナルの文字数を返しておく
         return originalCharNum;
         
     }@finally {
         
+        //自動開放プールを開放
         [pool drain];
         
     }
     
+    //入力可能文字数を返す
     return num;
 }
 
